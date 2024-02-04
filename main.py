1dc37663603538e7d18efacded26c1bc34b99742
tsvek <tsvek@gmail.com>
2024-02-04 12:30:52 +0100

    Rename back to main.py =) Add entering purchase info and save to separate csv file. Small fixes.

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import requests
 
 from bs4 import BeautifulSoup
 
-code = input("Entern bond's ID: ")
 
 def get_bond_data(bond_id) -> dict:
     """Parse bond's data from www.minfin.com.ua. Returns dictinary."""
-    url = f"https://index.minfin.com.ua/finance/bonds/{code}/"
+    url = f"https://index.minfin.com.ua/finance/bonds/{bond_id}/"
 
     response = requests.get(url=url)
     if response.status_code == 200:    
@@ -40,19 +39,39 @@ def bond_info_exists(bond_isin) -> bool:
         return bond_isin in isins
     return False
 
-def save_bond_info(info) -> None:
-    """Save bond information in csv file. Create new and write headers if file does'n exist."""
+def save_bond_info(info, action) -> None:
+    """Save bond information in csv file. Create new and write headers if file does'n exist. Dictinary and action(collect by default / purchase) requires."""
     mode = 'a+' if os.path.isfile('bonds_info.csv') else 'w'
-    with open("bonds_info.csv", mode=mode, encoding="utf-8", newline='') as file:
+    file_name = "bonds_info.csv" if action == 'collect' else 'purchased_info.csv'
+    done_print = f"Bond {info['ISIN']} information added." if action == 'collect' else f"Bond {info['ISIN']} purchase information for {info['Date']} has been saved."
+    with open(file_name, mode=mode, encoding="utf-8", newline='') as file:
         writer = csv.DictWriter(file, fieldnames=info.keys(), delimiter=';')
         if mode == 'w':
             writer.writeheader()
             print("Created new file with bonds information and added columns name.")
         writer.writerow(info)
-        print(f"Bond {info['ISIN']} information added.")
+        print(done_print)
 
-if bond_info_exists(code):
-    print(f"Bond {code} informations already exists.")
-else:
-    bond_info = get_bond_data(code)
-    save_bond_info(bond_info)
+game_on = True
+while game_on:
+    new = input("Do you want to add new purchase? Y or N: ")
+    if new.lower() == "n":
+        game_on = False
+        break
+
+    purchase_info = {
+        'ISIN': input("Enter bond's code(UAxxxxxxxxxx): "),
+        'Broker': input("Enter the broker name: "),
+        'Date': input("Enter date of purchase(dd.mm.YYYY): "),
+        'Price': float(input("Enter the bond's price: ")),
+        'Reinvest': float(input("Enter the amount of re-investment: ")),
+        'Number': int(input("Enter the number of bond purchased: ")),
+    }
+
+    save_bond_info(purchase_info, 'purchase')
+
+    if bond_info_exists(purchase_info['ISIN']):
+        print(f"Bond {purchase_info['ISIN']} informations already exists.")
+    else:
+        bond_info = get_bond_data(purchase_info['ISIN'])
+        save_bond_info(bond_info, 'collect')
