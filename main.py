2c87210f6551d8214a6e7e9050ed289d30e7eb01
tsvek <tsvek@gmail.com>
2024-02-25 15:14:04 +0100

    Add holding period, net income, income %, year yield and years payouts; function for entering.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import csv
+import json
 import os.path
 import requests
 
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+def input_data() -> dict:
+    """Enter the purchase info. Return dict."""
+    input_info = {
+        'ISIN': input("Enter bond's code(UAxxxxxxxxxx): "),
+        'Broker': input("Enter the broker name: "),
+        'Purchase_date': datetime.strptime(input("Enter date of purchase(dd.mm.YYYY): "), '%d.%m.%Y').date(), 
+        'Price': float(input("Enter the bond's price: ")),
+        'Number': int(input("Enter the number of bond purchased: ")),
+    }
+    input_info['Purchase_amount'] = round(purchase_info['Price']*purchase_info['Number'], 2)
+    input_info.update({
+        'Reinvest': float(input("Enter the amount of re-investment: ")),
+        'Fee': float(input("Enter the fee: "))
+    })
+    return input_data
 
 def get_bond_data(bond_id) -> dict:
     """Parse bond's data from www.minfin.com.ua. 
@@ -60,52 +76,20 @@ while game_on:
     if new.lower() == "n":
         game_on = False
         break
-    purchase_info = {
-        'ISIN': input("Enter bond's code(UAxxxxxxxxxx): "),
-        'Broker': input("Enter the broker name: "),
-        'Purchase_date': datetime.strptime(input("Enter date of purchase(dd.mm.YYYY): "), '%d.%m.%Y').date(), 
-        'Price': float(input("Enter the bond's price: ")),
-        'Number': int(input("Enter the number of bond purchased: ")),
-    }
-    purchase_info['Purchase_amount'] = round(purchase_info['Price']*purchase_info['Number'], 2)
-    purchase_info.update({
-        'Reinvest': float(input("Enter the amount of re-investment: ")),
-        'Fee': float(input("Enter the fee: "))
-    })
     
+    purchase_info = input_data()
     
-    url = f"https://index.minfin.com.ua/finance/bonds/{purchase_info['ISIN']}/"
-
-    response = requests.get(url=url)
     payout_dates = []
-    if response.status_code == 200:    
-        print(f"Bond {purchase_info['ISIN']} information found. Collecting...")
-        page = response.text
+    
+    purchase_info.update(get_bond_data(purchase_info['ISIN']))
 
-        soup = BeautifulSoup(page, "html.parser")
-        
-        content = soup.dl.find_all("dd")
-        payout_dates = [datetime.strptime(date.text, '%d.%m.%Y') for date in content[7] if '.' in date if datetime.strptime(date, '%d.%m.%Y').date() > purchase_info['Purchase_date']]
-        bond_data = {"Nominal_yield_%": float(content[2].text.replace("\xa0", '').replace(",", '.').replace('%', '')),
-                     "Payout_amount": float(content[3].text.replace("\xa0", '').replace(",", '.')),
-                     "Maturity_date" : datetime.strptime(content[6].text.replace("\xa0", '').replace(",", '.'), "%d.%m.%Y").date()
-        }       
-
-        print("Collecting succesfull.")
-    else:
-        print(f"Something going wrong.Status code: {response.status_code}")    
-        continue
-
-    purchase_info.update(bond_data)
-    profit_info = {
-        'Holding_period_days': (purchase_info['Maturity_date'] - purchase_info['Purchase_date']).days,
-        'Net_income': round(purchase_info['Number'] * (1000 + purchase_info['Payout_amount'] * len(payout_dates)) - purchase_info['Purchase_amount'], 2),
-    }
-    purchase_info.update(profit_info)
+    print("Calculating profits part...")
+    purchase_info['Holding_period_days'] = (purchase_info['Maturity_date'] - purchase_info['Purchase_date']).days
+    purchase_info['Net_income'] = round(purchase_info['Number'] * (1000 + purchase_info['Payout_amount'] * len(payout_dates)) - purchase_info['Purchase_amount'], 2)
     purchase_info['Income_%'] = round(purchase_info['Net_income']/purchase_info['Purchase_amount']*100, 2)
     purchase_info['Year_yield_%'] = round(purchase_info['Income_%']*365/purchase_info['Holding_period_days'], 2)
     #purchase_info['Status'] = 'Active' if date.today() < purchase_info['Maturity_date'] else purchase_info['Number'] * 1000
     for year in range(min(payout_dates).year, max(payout_dates).year + 1):
         purchase_info[f'Payout_yield_{year}'] = purchase_info['Number'] * purchase_info['Payout_amount'] * sum(1 for payout in payout_dates if payout.year == year)
-    
+    print('Done! Ready for save.')
     print(purchase_info)
