0ee8eb0c26878207eb9bb04143e5c1f0c5142ed5
tsvek <tsvek@gmail.com>
2024-03-02 17:18:05 +0100

    ---#---

--- db_manager.py
+++ db_manager.py
@@ -1,14 +1,55 @@
+import os.path
+import requests
 import sqlite3
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+def get_bond_data(bond_isin) -> dict:
+    """Parse bond's data from www.minfin.com.ua. 
+    Returns dictinary with nominal yeld, coupon payout and maturity date.
+    Also put actuasls payout dayes in global payout_dates list."""
+    url = f"https://index.minfin.com.ua/finance/bonds/{bond_isin}/"
+
+    response = requests.get(url=url)
+    payout_dates = list()
+    if response.status_code == 200:    
+        print(f"Bond {bond_isin} information found. Collecting...")
+        page = response.text
+
+        soup = BeautifulSoup(page, "html.parser")
+        
+        content = soup.dl.find_all("dd")
+        bond_data = {
+            "ISIN": bond_isin,
+            "Nominal_yield_%": float(content[2].text.replace("\xa0", '').replace(",", '.').replace('%', '')),
+            "Maturity_date" : datetime.strptime(content[6].text.replace("\xa0", '').replace(",", '.'), "%d.%m.%Y").date()
+        }       
+        payout_dates = [datetime.strptime(date.text, '%d.%m.%Y') for date in content[7] if '.' in date if datetime.strptime(date, '%d.%m.%Y').date() > new_purchase['Purchase_date']]
+        amount = float(content[3].text.replace("\xa0", '').replace(",", '.'))
+        payouts = {payout_date: amount for payout_date in payout_dates}
+
+        print("Collecting succesfull.")
+    else:
+        print(f"Something going wrong.Status code: {response.status_code}")    
+        return {}
+    return bond_data, payouts
 
 class DatabaseManager:
 
     def __init__(self, db_name='bonds_ua.db') -> None:
+        if not os.path.isfile(db_name):
+            self.create_tables(db_name)
         self.db_name = db_name
         self.conn = sqlite3.connect(self.db_name)
         self.cursor = self.conn.cursor()
 
-    def create_tables(self):
-        self.cursor.execute("""
+
+    def create_tables(self, db_name):
+        """Create database and tables."""
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("""
             create table if not exists Purchase_info(
                 ID int PRIMARY KEY,
                 ISIN char(12) not null,
@@ -20,7 +61,7 @@ class DatabaseManager:
                 Fee numeric(10, 2) default 0.0
             );
         """)
-        self.cursor.execute("""
+        cursor.execute("""
             create table if not exists Bonds_info(
                 ISIN char(12) PRIMARY KEY, 
                 Nominal_yield numeric(10, 2) not null,
@@ -28,7 +69,7 @@ class DatabaseManager:
                 FOREIGN KEY (ISIN) references Purchase_info (ISIN)
             );
         """)
-        self.cursor.execute("""
+        cursor.execute("""
             create table if not exists Payouts(
                 ID int PRIMARY KEY,
                 ISIN char(12) not null,
@@ -37,7 +78,7 @@ class DatabaseManager:
                 FOREIGN KEY (ISIN) references Bond_info (ISIN)
             );
         """)
-        self.cursor.execute("""
+        cursor.execute("""
             create table if not exists Exchange_rate(
                 ID int PRIMARY KEY,
                 Date date not null,
@@ -45,7 +86,7 @@ class DatabaseManager:
                 Rate_EUR numeric(10, 2)
             );
         """)
-        self.cursor.execute("""
+        cursor.execute("""
             create table if not exists Analytics(
                 ID int PRIMARY KEY,
                 Status char(15) not null, 
@@ -57,26 +98,38 @@ class DatabaseManager:
                 FOREIGN KEY (ID) references Purchase_info (ID)
             );
         """)
-        self.cursor.execute("""
+        cursor.execute("""
             create table if not exists Income_by_year(
                 ID int PRIMARY KEY,
                 ISIN char(12) not null,
                 FOREIGN KEY (ISIN) references Purchase_info (ISIN) 
             );
         """)
+        conn.commit()
         
     def add_purchase_row(self, info_list):
         query = "insert into Purchase_info (ISIN, Purchase_date, Quantity, Price, Reinvest, Broker, Fee) values (?, ?, ?, ?, ?, ?, ?);"
+        isin = info_list[0]
         self.cursor.execute(query, info_list)
         self.conn.commit()
+        print(f"New purchase from {info_list[1]} added to DataBase!")
+        if not self.bond_data_exists(isin):
+            bonds_info, payments = get_bond_data(isin)
+            self.add_bond_info(bonds_info)
+        else:
+            print(f"Info about bond {isin} is already in DataBase.")
+
+    def add_bond_info(self, data):
+        query = "insert into Bonds_info (ISIN, Nominal_yield, Maturity_date) value (?, ?, ?);"
+        self.cursor(query, data)
+        self.conn.commit()
+
+    def bond_data_exists(self, code):
+        query = "select * from Bonds_info where ISIN = ?;"
+        result = self.cursor.execute(query, code)
+        return True if len(result) > 0 else False
 
     def close_connection(self):
         self.cursor.close()
         self.conn.close()
-
-
-db = DatabaseManager()
-db.create_tables()
-db.close_connection()
-
     
